b2b278739e6996c58b8602231b49e22476558e29
Rakshit Sharma <kumarrakshit0402@gmail.com>
2026-06-11 19:09:49 +0530

    Project Structure

---
+++ src/main/java/org/dataengineering/datatierx/DataTierXApplication.java
@@ -0,0 +1,13 @@
+package org.dataengineering.datatierx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTierXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataTierXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dataengineering/datatierx/DataTierXApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dataengineering.datatierx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTierXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
