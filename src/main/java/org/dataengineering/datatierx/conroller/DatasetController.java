0bd800c3406f21add69becf90d57bb1fe44862e7
Rakshit Sharma <kumarrakshit0402@gmail.com>
2026-06-23 16:40:58 +0530

    Implemented Dataset Upload API

--- src/main/java/org/dataengineering/datatierx/conroller/DatasetController.java
+++ src/main/java/org/dataengineering/datatierx/conroller/DatasetController.java
@@ -6,7 +6,9 @@ import org.dataengineering.datatierx.service.DatasetService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,11 +28,20 @@ public class DatasetController {
 
     @GetMapping
     public List<DatasetMetadata> getAllDatasets() {
+
         return datasetService.getAllDatasets();
     }
 
     @GetMapping("/{id}")
     public DatasetMetadata getDataset(@PathVariable UUID id) {
+
         return datasetService.getDataset(id);
+    }
+
+    @PostMapping("/upload")
+    public DatasetMetadata uploadFile(
+            @RequestParam("file") MultipartFile file) throws IOException {
+
+        return datasetService.uploadFile(file);
     }
 }
